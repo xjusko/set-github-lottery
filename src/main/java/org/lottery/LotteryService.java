4e25bec8b9231658012d206ff530b680613ccdd1
xjusko <majus312@gmail.com>
2023-05-29 09:37:12 +0200

    mid commit removing github initialization

--- src/main/java/org/lottery/LotteryService.java
+++ src/main/java/org/lottery/LotteryService.java
@@ -58,64 +58,63 @@ public class LotteryService {
         issueTrackerConfigs.add(jiraService);
 
         List<RepositoryConfig> repositoryConfigs = new ArrayList<>();
-        repositoryConfigs.add(githubService);
+        //repositoryConfigs.add(githubService);
 
         List<StreamConfig> streamConfigs=new ArrayList<>();
         AphroditeConfig config = new AphroditeConfig(issueTrackerConfigs, repositoryConfigs, streamConfigs);
 
-        GitHub client = gitHubClientProvider.getApplicationClient();
-        GHApp app = client.getApp();
-        GHRepository repository = gitHubClientProvider.getInstallationClient(app.listInstallations().toList().get(0).getId()).getInstallation().listRepositories().toList().get(0);
-
-        Optional<Config> configFile = configFileProvider.fetchConfigFile(repository, Config.FILE_NAME, ConfigFile.Source.DEFAULT, Config.class);
-        configFile.ifPresent(value ->
-        {
-            for (Config.Participant participant: value.participants()) {
-                PagedIterable<GHIssue> queriedIssues = repository.queryIssues().assignee(participant.user()).list();
-                if (!queriedIssues.iterator().hasNext()) {
-                    try {
-                        GHIssueBuilder issueBuilder = repository.createIssue("title");
-                        issueBuilder.body(String.valueOf(participant.issueCount()));
-                        issueBuilder.assignee(participant.user());
-                        issueBuilder.create();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                } else {
-                    for (GHIssue issue : queriedIssues) {
-
-                        try {
-                          issue.comment(String.valueOf(participant.issueCount()));
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }
-
-            }
-        });
-
-
-
-
-
-
-//        Aphrodite aphrodite = Aphrodite.instance(config) ;
-//        SearchCriteria sc = new SearchCriteria.Builder()
-//                .setProduct("JBEAP")
-//                .setRelease(new Release("7.4.9.GA"))
-//                .build();
-//        List<Issue> result = aphrodite.searchIssues(sc);
-//        aphrodite.commentOnGithubIssue(new URL("https://github.com/xjusko/lottery-bot-test"), "test comment", "xjusko");
+//        GitHub client = gitHubClientProvider.getApplicationClient();
+//        GHApp app = client.getApp();
+//        GHRepository repository = gitHubClientProvider.getInstallationClient(app.listInstallations().toList().get(0).getId()).getInstallation().listRepositories().toList().get(0);
 //
+//        Optional<Config> configFile = configFileProvider.fetchConfigFile(repository, Config.FILE_NAME, ConfigFile.Source.DEFAULT, Config.class);
+//        configFile.ifPresent(value ->
+//        {
+//            for (Config.Participant participant: value.participants()) {
+//                PagedIterable<GHIssue> queriedIssues = repository.queryIssues().assignee(participant.user()).list();
+//                if (!queriedIssues.iterator().hasNext()) {
+//                    try {
+//                        GHIssueBuilder issueBuilder = repository.createIssue("title");
+//                        issueBuilder.body(String.valueOf(participant.issueCount()));
+//                        issueBuilder.assignee(participant.user());
+//                        issueBuilder.create();
+//                    } catch (IOException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                } else {
+//                    for (GHIssue issue : queriedIssues) {
 //
-//        StringBuilder issueComment = new StringBuilder();
-//        for (Issue url : result) {
-//            issueComment.append(url.getURL());
-//            issueComment.append("\n");
-//        }
-//        //System.out.println(issueComment);
-//        System.out.println(result.size());
+//                        try {
+//                          issue.comment(String.valueOf(participant.issueCount()));
+//                        } catch (IOException e) {
+//                            throw new RuntimeException(e);
+//                        }
+//                    }
+//                }
+//
+//            }
+//        });
+
+
+
+
+
+
+        Aphrodite aphrodite = Aphrodite.instance(config) ;
+        SearchCriteria sc = new SearchCriteria.Builder()
+                .setProduct("JBEAP")
+                .setRelease(new Release("7.4.9.GA"))
+                .build();
+        List<Issue> result = aphrodite.searchIssues(sc);
+
+
+        StringBuilder issueComment = new StringBuilder();
+        for (Issue url : result) {
+            issueComment.append(url.getURL());
+            issueComment.append("\n");
+        }
+        //System.out.println(issueComment);
+        System.out.println(result.size());
 
     }
 
